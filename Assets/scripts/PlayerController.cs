95f4fca4488d2c5c529f3b897625747d0ab5aefd
denisgillespie <denisjgillespie@hotmail.com>
2022-07-04 22:55:33 +0100

    Updated player script

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -2,38 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
     Rigidbody rigidbody;
+
+    Material material;
+
     public float speed = 5f;
-
     public float jump = 10f;
-    bool canJump;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 1.5f;
 
-    void Start()
-    {
+    void Start() {
         rigidbody = GetComponent<Rigidbody>();
-        canJump = true;
+        material = GetComponent<Renderer>().material;
     }
 
-    void FixedUpdate()
-    {
+    void Update() {
+        if (Input.GetButtonDown("Jump")) {
+            rigidbody.velocity = Vector3.up * jump;
+        }
 
-        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        rigidbody.MovePosition(transform.position + input * Time.deltaTime * speed);
+        #region falling
+        if (rigidbody.velocity.y < 0) {
+            rigidbody.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        } else if (rigidbody.velocity.y > 0 && !Input.GetButton("Jump")) {
+            rigidbody.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+        #endregion
+    }
 
-        if(Input.GetKeyUp(KeyCode.Space) && canJump)
-        {
-            Debug.Log("cant jump");
-            canJump = false;
-            Vector3 jumpPos = new Vector3(0, jump, 0);
-            rigidbody.AddForce(jumpPos);
+    private void FixedUpdate() {
+
+        if (Input.GetButton("Horizontal")) {
+            Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+            rigidbody.MovePosition(transform.position + input * Time.deltaTime * speed);
         }
     }
 
-    void OnCollisionEnter()
-    {
-        Debug.Log("canjump");
-        canJump = true;
-    }
 }
